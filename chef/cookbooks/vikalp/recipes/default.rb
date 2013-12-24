bda8561a2c48d27c9b555b87810c5c28f1b38a70
Mihir <mihir.khatwani@gmail.com>
2013-12-24 21:40:25 +0530

    Changed the deployment script

--- chef/cookbooks/vikalp/recipes/default.rb
+++ chef/cookbooks/vikalp/recipes/default.rb
@@ -4,8 +4,16 @@ execute "npm install -g less" do
 	not_if "ls /usr/bin/lessc"
 end
 
+package "zlib-devel"
+package "libjpeg-devel"
+package "freetype-devel"
+
+execute "sudo ln -s /usr/lib64/libjpeg.so /usr/lib" do
+	not_if do ::File.exists?('/usr/lib/libjpeg.so') end
+end
+
 execute "pip install -r requirements.txt" do
   cwd "/vagrant"
 end
 
-package "nginx"
+package "nginx"	
