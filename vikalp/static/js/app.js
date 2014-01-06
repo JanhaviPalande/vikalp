dbceb2e3683eb6a22fe56851a4545808bae25554
Krunal Patel <kp_ptl@yahoo.com>
2014-01-06 17:12:11 +0530

    Bootstrap related changes to index.html

--- vikalp/static/js/app.js
+++ vikalp/static/js/app.js
@@ -1,83 +1,112 @@
 $(document).ready( function() {
     
-	$.fn.smint = function( options ) {
+//	$.fn.smint = function( options ) {
+//
+//		// adding a class to users div
+//		$(this).addClass('smint')
+//
+//		var settings = $.extend({
+//		            'scrollSpeed '  : 500
+//		}, options);
+//
+//		//Set the variables needed
+//		//var optionLocs = new Array();
+//		var lastScrollTop = 0;
+//		var menuHeight = $(".smint").height();
+//
+//		return $('.smint a').each( function(index) {
+//
+//			if ( settings.scrollSpeed ) {
+//				var scrollSpeed = settings.scrollSpeed
+//			}
+//
+//
+//			var stickyTop = $('.smint').offset().top;
+//
+//
+//			// check position and make sticky if needed
+//			var stickyMenu = function(direction){
+//
+//				// current distance top
+//				var scrollTop = $(window).scrollTop();
+//
+//				//console.log('Sticky Top:' + stickyTop);
+//				//console.log('Scroll Top:' + scrollTop);
+//
+//				// if we scroll more than the navigation, change its position to fixed and add class 'fxd', otherwise change it back to absolute and remove the class
+//                console.log('scrolltop' + scrollTop);
+//				if (scrollTop > stickyTop) {
+//					$('.smint').css({ 'position': 'fixed', 'top':0 }).addClass('fxd');
+//
+//				} else if(scrollTop < 100){
+//
+//					$('.smint').css({ 'position': 'absolute', 'top':'100px' }).removeClass('fxd');
+//
+//				}
+//
+//			};
+//
+//			// run functions
+//			stickyMenu();
+//
+//			// run function every time you scroll
+//			$(window).scroll(function() {
+//				//Get the direction of scroll
+//				var st = $(this).scrollTop();
+//				if (st > lastScrollTop) {
+//				    direction = "down";
+//				} else if (st < lastScrollTop ){
+//				    direction = "up";
+//				}
+//				lastScrollTop = st;
+//				stickyMenu(direction);
+//
+//			});
+//
+//		});
+//	};
 
-		// adding a class to users div
-		$(this).addClass('smint')
-
-		var settings = $.extend({
-		            'scrollSpeed '  : 500
-		}, options);
-
-		//Set the variables needed
-		//var optionLocs = new Array();
-		var lastScrollTop = 0;
-		var menuHeight = $(".smint").height();
-
-		return $('.smint a').each( function(index) {
-            
-			if ( settings.scrollSpeed ) {
-				var scrollSpeed = settings.scrollSpeed
-			}
-
-
-			var stickyTop = $('.smint').offset().top;
-				
-
-			// check position and make sticky if needed
-			var stickyMenu = function(direction){
-
-				// current distance top
-				var scrollTop = $(window).scrollTop(); 
-
-				//console.log('Sticky Top:' + stickyTop);
-				//console.log('Scroll Top:' + scrollTop);
-				
-				// if we scroll more than the navigation, change its position to fixed and add class 'fxd', otherwise change it back to absolute and remove the class
-				if (scrollTop > stickyTop) { 
-					$('.smint').css({ 'position': 'fixed', 'top':0 }).addClass('fxd');	
-					
-				} else if(scrollTop < 100){
-					
-					$('.smint').css({ 'position': 'absolute', 'top':'100px' }).removeClass('fxd'); 
-					
-				}   
-
-			};
-
-			// run functions
-			stickyMenu();
-
-			// run function every time you scroll
-			$(window).scroll(function() {
-				//Get the direction of scroll
-				var st = $(this).scrollTop();
-				if (st > lastScrollTop) {
-				    direction = "down";
-				} else if (st < lastScrollTop ){
-				    direction = "up";
-				}
-				lastScrollTop = st;
-				stickyMenu(direction);
-
-			});
-        	
-		});
-	};
 
 
 
+//    $('.subMenu').smint({
+//    	'scrollSpeed' : 1000
+//    });
 
-    $('.subMenu').smint({
-    	'scrollSpeed' : 500
+    $(window).scroll(function() {
+
+        var scrollTop = $(window).scrollTop();
+        var stickyTop = $('.navbar-fixed-top').offset().top;
+
+
+        if (scrollTop > stickyTop) {
+					$('.navbar-fixed-top').css({ 'position': 'fixed', 'top':0 }).addClass('fxd');
+
+		} else if(scrollTop < 100){
+
+					$('.navbar-fixed-top').css({ 'position': 'absolute', 'top':'100px' }).removeClass('fxd');
+
+		}
+
+       if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+          $("#footer .list-group-item").show();
+       }else{
+           $("#footer .list-group-item").hide();
+       }
     });
 
 
+    $('a#feature-article').hover(function(){
+
+
+        $(this).find('p').toggle();
+    });
     /*Footer*/
 
-    $("#footer-lower").hide();
-	$("footer").click(function(){
-		$("#footer-lower").toggle();
+    $("#footer .list-group-item").hide();
+
+	$("#footer").on('click',function(){
+		$("#footer .list-group-item").toggle();
 	});
-	
+//
 });
