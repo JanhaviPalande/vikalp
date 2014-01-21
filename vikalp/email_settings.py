3be5667b3ef32129179b8afee575b1b96dcc0069
Mihir <mihir.khatwani@gmail.com>
2014-01-21 17:20:20 +0530

    Email settings seggregated

---
+++ vikalp/email_settings.py
@@ -0,0 +1,13 @@
+##################
+# EMAIL SETTINGS #
+##################
+
+EMAIL_BACKEND = 'django_mandrill.mail.backends.mandrillbackend.EmailBackend'
+MANDRILL_API_KEY = 'sMPZL3IzNAfpAw03VQQ3Fg'
+DEFAULT_FROM_EMAIL = "do-no-reply@vikalpindia.org"
+#
+# EMAIL_HOST_USER = 'vikalpgithub@gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.mandrillapp.com'
+# EMAIL_HOST_PASSWORD = 'sMPZL3IzNAfpAw03VQQ3Fg'
+# EMAIL_PORT = 587
