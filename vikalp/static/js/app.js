64e8d63b92582d8081771ae2ed455291f924fcfe
Chandan <chandan.ks@outlook.com>
2014-02-11 15:27:34 +0530

    fixed carousel resizing bug on different resolutions, added jquery for it, will need to refresh the homepage to see the change.

--- vikalp/static/js/app.js
+++ vikalp/static/js/app.js
@@ -1,153 +1,91 @@
-var FixCategoryHeight = (function(){
-    function setHeight(){
-        $(".category-page-category-list-wrapper").each(function(){
+var FixCategoryHeight = (function () {
+    function setHeight() {
+        $(".category-page-category-list-wrapper").each(function () {
             var ele = $(this),
                 maxHHeight = 0,
                 maxPHeight = 0;
 
-            ele.find('h4').each(function(){
-                if(maxHHeight < $(this).innerHeight()){
+            ele.find('h4').each(function () {
+                if (maxHHeight < $(this).innerHeight()) {
                     maxHHeight = $(this).innerHeight();
                 }
             });
-            ele.find('p').each(function(){
-                if(maxPHeight < $(this).innerHeight()){
+            ele.find('p').each(function () {
+                if (maxPHeight < $(this).innerHeight()) {
                     maxPHeight = $(this).innerHeight();
                 }
             });
 
-            ele.find('h4').each(function(){
+            ele.find('h4').each(function () {
                 $(this).css("height", maxHHeight + "px");
             });
-            ele.find('p').each(function(){
+            ele.find('p').each(function () {
                 $(this).css("height", maxPHeight + "px");
             });
         });
     }
 
     return {
-      init: function(){
-          setHeight();
-      }
+        init: function () {
+            setHeight();
+        }
     };
 })();
 
 
-$(document).ready( function() {
-    
-//	$.fn.smint = function( options ) {
-//
-//		// adding a class to users div
-//		$(this).addClass('smint')
-//
-//		var settings = $.extend({
-//		            'scrollSpeed '  : 500
-//		}, options);
-//
-//		//Set the variables needed
-//		//var optionLocs = new Array();
-//		var lastScrollTop = 0;
-//		var menuHeight = $(".smint").height();
-//
-//		return $('.smint a').each( function(index) {
-//
-//			if ( settings.scrollSpeed ) {
-//				var scrollSpeed = settings.scrollSpeed
-//			}
-//
-//
-//			var stickyTop = $('.smint').offset().top;
-//
-//
-//			// check position and make sticky if needed
-//			var stickyMenu = function(direction){
-//
-//				// current distance top
-//				var scrollTop = $(window).scrollTop();
-//
-//				//console.log('Sticky Top:' + stickyTop);
-//				//console.log('Scroll Top:' + scrollTop);
-//
-//				// if we scroll more than the navigation, change its position to fixed and add class 'fxd', otherwise change it back to absolute and remove the class
-//                console.log('scrolltop' + scrollTop);
-//				if (scrollTop > stickyTop) {
-//					$('.smint').css({ 'position': 'fixed', 'top':0 }).addClass('fxd');
-//
-//				} else if(scrollTop < 100){
-//
-//					$('.smint').css({ 'position': 'absolute', 'top':'100px' }).removeClass('fxd');
-//
-//				}
-//
-//			};
-//
-//			// run functions
-//			stickyMenu();
-//
-//			// run function every time you scroll
-//			$(window).scroll(function() {
-//				//Get the direction of scroll
-//				var st = $(this).scrollTop();
-//				if (st > lastScrollTop) {
-//				    direction = "down";
-//				} else if (st < lastScrollTop ){
-//				    direction = "up";
-//				}
-//				lastScrollTop = st;
-//				stickyMenu(direction);
-//
-//			});
-//
-//		});
-//	};
+var FixCarousalHeight = (function () {
+    function setHeight() {
+        console.log("Hurray !!!!!!!!!!");
+        var featureListHeight = $(".featured-list").innerHeight();
+        $(".fill-image").css("height", featureListHeight);
+    }
 
+    function trigger() {
+        if ($(".featured-list").offset().left > 200) {
+            var numberOfImages = $(".featured-list img").length;
+            $(".featured-list img").each(function () {
+                console.log($(this));
+                $(this).load(function () {
+                    console.log(numberOfImages)
+                    numberOfImages = numberOfImages - 1;
+                    if (numberOfImages === 0) {
+                        setHeight();
+                    }
+                });
+            });
+        }
+    }
 
+    return{
+        init: function () {
+            trigger();
+        }
+    }
+})();
 
+var PageEventsList = (function () {
+    function resizeHandler() {
+        FixCarousalHeight.init();
+    }
 
-//    $('.subMenu').smint({
-//    	'scrollSpeed' : 1000
-//    });
+    return {
+        init: function () {
+            $(window).resize(function () {
+                resizeHandler();
+            });
 
-     //$('#top-nav').css({ 'position': 'absolute', 'top':'100px' }).removeClass('fxd');
-//    console.log('Loaded');
-    $(window).scroll(function() {
+        }
+    }
 
-//        var scrollTop = $(window).scrollTop();
-//
-//        var stickyTop = $('.navbar-fixed-top').offset().top;
-//
-//        //console.log(stickyTop);
-//
-//        if (scrollTop > stickyTop) {
-//					$('.navbar-fixed-top').css({ 'position': 'fixed', 'top':0 });
-//
-//		} else if(scrollTop < 100){
-//
-//					$('.navbar-fixed-top').css({ 'position': 'absolute', 'top':'100px' });
-//
-//		}
+})();
 
-//       if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-//          $("#footer .list-group-item").show();
-//       }else{
-//           $("#footer .list-group-item").hide();
-//       }
-    });
-
-
-    $('a#feature-article').hover(function(){
-
-
+$(document).ready(function () {
+    $('a#feature-article').hover(function () {
         $(this).find('p').toggle();
     });
 
-    /*Footer*/
-
-//    $("#footer .list-group-item").hide();
-
-//	$("#footer").on('click',function(){
-//		$("#footer .list-group-item").toggle();
-//	});
 
     FixCategoryHeight.init();
+    FixCarousalHeight.init();
+    PageEventsList.init();
 });
