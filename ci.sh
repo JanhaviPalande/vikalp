70f5759bdc4e4d1a06035161b830411db12740d1
mihirk <mihir.khatwani@gmail.com>
2014-02-13 18:02:34 +0530

    removed ci.sh

--- ci.sh
+++
@@ -1,9 +0,0 @@
-cd chef/
-if ! type -p chef-solo > /dev/null; then
-	sudo bash install.sh
-fi
-sudo chef-solo -j node.json -c solo.rb
-cd ../
-sudo pip install -r requirements.txt
-sudo python manage.py  syncdb --noinput --settings=vikalp.local_settings
-sudo python manage.py test vikalp --settings=vikal.local_settings
