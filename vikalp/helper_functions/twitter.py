e89cedcff5745618f23a88c0cdb6491e4ca2ca00
Chandan <chandan.ks@outlook.com>
2014-02-20 16:19:28 +0530

    testing.

--- vikalp/helper_functions/twitter.py
+++ vikalp/helper_functions/twitter.py
@@ -1,14 +1,14 @@
-from os import environ, getenv
+import os
 from tweepy import OAuthHandler, API
 
 
 def post_to_twitter(tweet_message="Visit www.vikalpsangam.org"):
-    app_key = environ.get('TWITTER_APP_KEY')
-    print environ.get('TWITTER_APP_KEY')
-    print getenv('TWITTER_APP_KEY', None)
-    app_secret = environ.get('TWITTER_APP_SECRET')
-    oauth_token = environ.get('TWITTER_OAUTH_TOKEN')
-    oauth_token_secret = environ.get('TWITTER_OAUTH_TOKEN_SECRET')
+    app_key = os.environ.get('TWITTER_APP_KEY')
+    print os.environ.get('TWITTER_APP_KEY')
+    print os.getenv('TWITTER_APP_KEY', "Random!")
+    app_secret = os.environ.get('TWITTER_APP_SECRET')
+    oauth_token = os.environ.get('TWITTER_OAUTH_TOKEN')
+    oauth_token_secret = os.environ.get('TWITTER_OAUTH_TOKEN_SECRET')
 
     auth = OAuthHandler(app_key, app_secret)
     auth.set_access_token(oauth_token, oauth_token_secret)
