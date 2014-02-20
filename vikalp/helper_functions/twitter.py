b264103087318b1b1217b3d4f4fd1e82f2c098a6
Chandan <chandan.ks@outlook.com>
2014-02-20 15:37:50 +0530

    still testing.

--- vikalp/helper_functions/twitter.py
+++ vikalp/helper_functions/twitter.py
@@ -2,23 +2,21 @@ import os
 from tweepy import OAuthHandler, API
 
 
-def post_to_twitter(tweet_message="Visit www.vikalpsangam.org"):
-    app_key = os.environ.get('TWITTER_APP_KEY')
-    app_secret = os.environ.get('TWITTER_APP_SECRET')
-    oauth_token = os.environ.get('TWITTER_OAUTH_TOKEN')
-    oauth_token_secret = os.environ.get('TWITTER_OAUTH_TOKEN_SECRET')
+def post_to_twitter(self, tweet_message="Visit www.vikalpsangam.org"):
+    self.app_key = os.environ.get('TWITTER_APP_KEY')
+    self.app_secret = os.environ.get('TWITTER_APP_SECRET')
+    self.oauth_token = os.environ.get('TWITTER_OAUTH_TOKEN')
+    self.oauth_token_secret = os.environ.get('TWITTER_OAUTH_TOKEN_SECRET')
 
-    auth = OAuthHandler(app_key, app_secret)
-    auth.set_access_token(oauth_token, oauth_token_secret)
+    auth = OAuthHandler(self.app_key, self.app_secret)
+    auth.set_access_token(self.oauth_token, self.oauth_token_secret)
 
     api_stream = API(auth)
 
     try:
         api_stream.update_status(tweet_message)
     except Exception as e:
-        print app_key
-        print app_secret
-        print oauth_token
-        print oauth_token_secret
+        print self.app_key
+        print self.app_secret
         print tweet_message
         print "Exception log: " + str(e)
