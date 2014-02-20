47cea861368553543b87a8895776c2e4c273225e
mihirk <mihir.khatwani@gmail.com>
2014-02-20 18:24:43 +0530

    Added description from about us page to the homepage

--- vikalp/views/home_page_view.py
+++ vikalp/views/home_page_view.py
@@ -1,3 +1,4 @@
+from copy import deepcopy, copy
 from django.shortcuts import render
 from mezzanine.conf import settings
 from vikalp.helper_functions.functional import get_page
@@ -5,13 +6,25 @@ from vikalp.views.views import articleService
 
 
 class HomePage:
-    def get_context_for_promoted_articles(self, article_promoted_list, carousel_content, categories, latest_articles, page):
+    def get_context_for_promoted_articles(self, article_promoted_list, carousel_content, categories, latest_articles, page, about_page_description):
         return {"promoted_articles": article_promoted_list, "carousel_content": carousel_content,
-                "categories": categories, "latest_articles": latest_articles, "page": page}
+                "categories": categories, "latest_articles": latest_articles, "page": page, "about_page_description": about_page_description}
+
+    def about_us_page(self, request):
+        dummy_request = copy(request)
+        dummy_request.path_info = "/about/"
+        about_page = get_page(dummy_request)
+        return about_page
 
     def promoted_article_on_homepage(self, request, template="pages/index.html"):
         settings.use_editable()
+        about_page = self.about_us_page(request)
+        if(about_page):
+            about_page_description = about_page.description
+        else:
+            about_page_description = None
         return render(request, template, self.get_context_for_promoted_articles(articleService.get_promoted_articles(),
                                                                                 articleService.get_carousel_content(),
                                                                                 articleService.get_all_article_categories(),
-                                                                                articleService.get_latest_articles(), get_page(request)))
+                                                                                articleService.get_latest_articles(),
+                                                                                get_page(request), about_page_description))
