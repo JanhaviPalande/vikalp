04f89900f2d31056523835816b1c6eebf878114d
Chandan <chandan.ks@outlook.com>
2014-02-20 14:44:07 +0530

    testing.

--- vikalp/admin.py
+++ vikalp/admin.py
@@ -30,7 +30,8 @@ class ArticleAdmin(BlogPostAdmin, DisplayableAdmin):
         last_updated = obj.updated
         obj.save()
         if not last_updated or (last_updated and ((obj.updated - last_updated) > timedelta(hours=24))):
-            post_to_twitter("'" + obj.title + "' " + request.get_host() + obj.get_absolute_url())
+            # post_to_twitter("'" + obj.title + "' " + request.get_host() + obj.get_absolute_url())
+            post_to_twitter("'" + obj.title + "' " + "vikalpstaging.twhosted.com" + obj.get_absolute_url())
 
     def save_form(self, request, form, change):
         OwnableAdmin.save_form(self, request, form, change)
