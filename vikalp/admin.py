a1f9f011ebb01c555f74b79d848b1fea9c0e8d7e
Chandan <chandan.ks@outlook.com>
2014-02-21 14:53:56 +0530

    disables auto sharing if admin has not yet assigned published status to a story.

--- vikalp/admin.py
+++ vikalp/admin.py
@@ -30,7 +30,8 @@ class ArticleAdmin(BlogPostAdmin, DisplayableAdmin):
         last_updated = obj.updated
         obj.save()
         if not last_updated or (last_updated and ((obj.updated - last_updated) > timedelta(hours=24))):
-            post_to_twitter(tweet_message="'" + obj.title + "' " + request.get_host() + obj.get_absolute_url())
+            if str(obj.get_status_display()) == 'Published':
+                post_to_twitter(tweet_message="'" + obj.title + "' " + request.get_host() + obj.get_absolute_url())
 
     def save_form(self, request, form, change):
         OwnableAdmin.save_form(self, request, form, change)
