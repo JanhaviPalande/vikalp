afb1bd4eac1dd794c8bb98e2fd2858fa320bf29a
Chandan <chandan.ks@outlook.com>
2014-02-25 16:38:09 +0530

    now reading api keys and oauth tokens for pushing to twitter from a text file, so added twitter.py back to VCS.

--- vikalp/helper_functions/twitter.py
+++ vikalp/helper_functions/twitter.py
@@ -1,14 +1,15 @@
-import os
 from tweepy import OAuthHandler, API
 
 
 def post_to_twitter(tweet_message="Visit www.vikalpsangam.org"):
-    app_key = os.environ.get('TWITTER_APP_KEY')
-    app_secret = os.environ.get('TWITTER_APP_SECRET')
-    oauth_token = os.environ.get('TWITTER_OAUTH_TOKEN')
-    oauth_token_secret = os.environ.get('TWITTER_OAUTH_TOKEN_SECRET')
+    keys = open("vikalp/twitter_keys.txt", "r").read().split(',')
 
-    auth = OAuthHandler(app_key, app_secret)
+    api_key = keys[0]
+    api_secret = keys[1]
+    oauth_token = keys[2]
+    oauth_token_secret = keys[3]
+
+    auth = OAuthHandler(api_key, api_secret)
     auth.set_access_token(oauth_token, oauth_token_secret)
 
     api_stream = API(auth)
