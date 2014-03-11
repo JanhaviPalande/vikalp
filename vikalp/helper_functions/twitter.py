89f11516cdccd44ee31e9ef86ef28ff252c64c94
Chandan <chandank@INchandank.local>
2014-03-11 15:05:23 +0530

    ignored twitter_keys.txt from version control, changed post_to_twitter function to handle errors due to unavailable tokens.

--- vikalp/helper_functions/twitter.py
+++ vikalp/helper_functions/twitter.py
@@ -4,15 +4,18 @@ from tweepy import OAuthHandler, API
 def post_to_twitter(tweet_message="Visit www.vikalpsangam.org"):
     keys = open("vikalp/twitter_keys.txt", "r").read().split(',')
 
-    api_key = keys[0]
-    api_secret = keys[1]
-    oauth_token = keys[2]
-    oauth_token_secret = keys[3]
+    try:
+        api_key = keys[0]
+        api_secret = keys[1]
+        oauth_token = keys[2]
+        oauth_token_secret = keys[3]
 
-    auth = OAuthHandler(api_key, api_secret)
-    auth.set_access_token(oauth_token, oauth_token_secret)
+        auth = OAuthHandler(api_key, api_secret)
+        auth.set_access_token(oauth_token, oauth_token_secret)
 
-    api_stream = API(auth)
+        api_stream = API(auth)
+    except:
+        pass
 
     try:
         api_stream.update_status(tweet_message)
