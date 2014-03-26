ab6fcf0a603dbbb79f60d3b4d5c127b8e21308bc
Chandan <chandan.ks@outlook.com>
2014-03-26 11:23:51 +0530

    increasing time delta between automatic sharing of a story from 24 hrs. to 48 hrs.

--- vikalp/admin.py
+++ vikalp/admin.py
@@ -30,7 +30,7 @@ class ArticleAdmin(BlogPostAdmin, DisplayableAdmin):
         last_updated = obj.updated
         obj.save()
         if str(obj.get_status_display()) == 'Published':
-            if not last_updated or (last_updated and ((obj.updated - last_updated) > timedelta(hours=24))):
+            if not last_updated or (last_updated and ((obj.updated - last_updated) > timedelta(hours=48))):
                 post_to_twitter(tweet_message="'" + obj.title + "' " + "vikalpsangam.org" + obj.get_absolute_url())
 
     def save_form(self, request, form, change):
