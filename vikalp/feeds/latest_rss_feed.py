874319c9832970dd8cdd8c08356696963364652e
Dinesh Kumar <dineshkb@thoughtworks.com>
2014-09-25 14:06:42 +0530

    Excluding articles which are drafts, and having future publish_date

--- vikalp/feeds/latest_rss_feed.py
+++ vikalp/feeds/latest_rss_feed.py
@@ -1,6 +1,9 @@
 from django.contrib.syndication.views import Feed
 from django.utils.feedgenerator import Rss201rev2Feed
+from mezzanine.core.models import CONTENT_STATUS_PUBLISHED
+import pytz
 from vikalp.models import Article
+import datetime
 
 
 class ExtendedRssFeed(Rss201rev2Feed):
@@ -17,8 +20,9 @@ class LatestFeeds(Feed):
 
     def items(self):
         articles_to_fetch = 15
-        feed_articles = Article.objects.filter(add_to_carousel=False)
-        return feed_articles.order_by('-publish_date')[:articles_to_fetch]
+        all_articles = Article.objects.filter(add_to_carousel=False, status=CONTENT_STATUS_PUBLISHED)
+        published_articles = all_articles.filter(publish_date__lt=datetime.datetime.utcnow().replace(tzinfo=pytz.utc))
+        return published_articles.order_by('-publish_date')[:articles_to_fetch]
 
     def item_title(self, item):
         return item.title
