8ce3158df8e5ce759c4fa95b00f09b6be2bfed45
Suraj Babar <surajb@INsurajbab.local>
2015-01-13 08:44:39 +0530

    #1009 : Suraj : On article page - Added share media tag after comments and read more article image.

--- vikalp/static/js/app.js
+++ vikalp/static/js/app.js
@@ -252,3 +252,18 @@ $(window).load(function () {
     SetPageSeparatorBar.init();
     CommentsUI.init();
 });
+
+
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+    $('body').on('click', function (e) {
+        $('[data-toggle="popover"]').each(function () {
+            //the 'is' for buttons that trigger popups
+            //the 'has' for icons within a button that triggers a popup
+            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                $(this).popover('hide');
+            }
+        });
+    });
+})
